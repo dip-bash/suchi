800c1a0d7684e106588232ab253330193e5c2a1e
SAUMYADIP <saumyadip.social@gmail.com>
2026-06-02 17:48:06 +0530

    fix uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -47,3 +47,4 @@ echo "Uninstallation complete."
 // Random modification at 1787029216.819801
 // Random modification at 1787029216.842436
 // Random modification at 1787029216.880176
+// Random modification at 1787029216.894231
