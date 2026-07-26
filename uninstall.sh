682b084eee1ecb4c54eca7c93d8b1bb59935835f
SAUMYADIP <saumyadip.social@gmail.com>
2026-07-27 04:21:28 +0530

    optimise uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -45,6 +45,5 @@ echo "Uninstallation complete."
 // Random modification at 1787029216.797292
 // Random modification at 1787029216.812298
 // Random modification at 1787029216.819801
-// Random modification at 1787029216.842436
 // Random modification at 1787029216.880176
 // Random modification at 1787029216.894231
